93a1c77833ea418779d6ae97c734e168a220947b
matt <matt@dont-ping.me>
2022-06-20 08:17:48 -0700

    feat(entry): change base_path for non-pyinstaller.

--- blueberry.py
+++ blueberry.py
@@ -18,7 +18,7 @@ def resource_path(relative_path):
         # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
     except Exception:
-        base_path = os.path.abspath(".")
+        base_path = ROOT_DIR
 
     return os.path.join(base_path, relative_path)
 
